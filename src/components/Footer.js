99b8c4dc7c27482d0e9bb1f21f105f2daf5c0e20
William Crownover <williamcrownover1@gmail.com>
2021-10-02 00:02:46 -0700

    Imported font awesome logos and styled footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faVimeo } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
 
 	return (
 		<footer>
-			<h3>Footer</h3>
+			<a href="https://github.com/WilliamCrownover" target="_blank" rel="noreferrer">
+				<FontAwesomeIcon icon={faGithubSquare} size="3x" />
+			</a>
+			<a href="https://www.linkedin.com/in/williamcrownover/" target="_blank" rel="noreferrer">
+				<FontAwesomeIcon icon={faLinkedin} size="3x" />
+			</a>
+			<a href="https://vimeo.com/user12491647" target="_blank" rel="noreferrer">
+				<FontAwesomeIcon icon={faVimeo} size="3x" />
+			</a>
 		</footer>
 	);
 }
