c4cc8698efddfdd2ddabf9459de31c95b6871352
William Crownover <williamcrownover1@gmail.com>
2021-10-02 20:28:21 -0700

    Enhanced contact form validation and conditional alert message rendering

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,6 +5,14 @@ export default function Contact() {
 	const [visitorName, setName] = useState('');
 	const [visitorEmail, setEmail] = useState('');
 	const [visitorMessage, setMessage] = useState('');
+	const [validName, setValidName] = useState(false);
+	const [validEmail, setValidEmail] = useState(false);
+	const [validMessage, setValidMessage] = useState(false);
+	const [leftName, setLeftName] = useState(false);
+	const [leftEmail, setLeftEmail] = useState(false);
+	const [leftMessage, setLeftMessage] = useState(false);
+	const nameRegex = new RegExp(/^[A-Za-z_]+\s?[A-Za-z_]+$/);
+	const emailRegex = new RegExp(/^[A-Za-z0-9.]+@[A-Za-z_]+\.[A-Za-z]{2,3}$/);
 
 	const handleInputChange = (e) => {
 		const { target } = e;
@@ -20,6 +28,35 @@ export default function Contact() {
 		}
 	};
 
+	const checkInput = (e) => {
+		const { target } = e;
+		const inputType = target.name;
+		const inputValue = target.value;
+
+		if(inputType === 'visitorName') {
+			setLeftName(true);
+			if(nameRegex.test(inputValue)) {
+				setValidName(true);
+			} else {
+				setValidName(false);
+			}
+		} else if (inputType === 'visitorEmail') {
+			setLeftEmail(true);
+			if(emailRegex.test(inputValue)) {
+				setValidEmail(true);
+			} else {
+				setValidEmail(false);
+			}
+		} else if (inputType === 'visitorMessage') {
+			setLeftMessage(true);
+			if(inputValue) {
+				setValidMessage(true)
+			} else {
+				setValidMessage(false);
+			}
+		}
+	}
+
 	return (
 		<div className="page-section">
 			<h2 className="lines">Contact</h2>
@@ -30,6 +67,7 @@ export default function Contact() {
 					value={visitorName}
 					name="visitorName"
 					onChange={handleInputChange}
+					onBlur={checkInput}
 					type="text"
 					placeholder="Your Name"
 				/>
@@ -39,7 +77,8 @@ export default function Contact() {
 					value={visitorEmail}
 					name="visitorEmail"
 					onChange={handleInputChange}
-					type="text"
+					onBlur={checkInput}
+					type="email"
 					placeholder="Your Email"
 				/>
 				<label htmlFor="message-entry">Message</label>
@@ -48,8 +87,21 @@ export default function Contact() {
 					value={visitorMessage}
 					name="visitorMessage"
 					onChange={handleInputChange}
+					onBlur={checkInput}
 					type="text"
 				/>
+				{(!validName && leftName) &&
+					<h5>Please enter a valid name</h5>
+				}
+				{(!validEmail && leftEmail) &&
+					<h5>Please enter a valid email</h5>
+				}
+				{(!validMessage && leftMessage) &&
+					<h5>Please write a message to send</h5>
+				}
+				{(validName && validEmail && validMessage) 
+					? <button type="submit">Send</button>
+					: <button type="submit" disabled>Send</button>}
 			</form>
 		</div>
 	);
