2e02e360b97e76481bc21803148f190419ae5c74
William Crownover <williamcrownover1@gmail.com>
2021-10-02 22:45:05 -0700

    Fixed alt text in project links

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,9 +9,9 @@ export default function Project( { title, imageName, liveURL, repoURL } ) {
 				<img src={`${process.env.PUBLIC_URL}/assets/${imageName}`} alt="Project"/>
 				<div className="project-btn-wrapper">
 					<a className="btn-left" href={liveURL} target="_blank" rel="noreferrer"
-						alt="Go to Shelf-Life Webpage">Live Site</a>
+						alt={`Go to ${title} Webpage`}>Live Site</a>
 					<a className="btn-right" href={repoURL} target="_blank" rel="noreferrer"
-						alt="Go to Shelf-Life Repo">Repository</a>
+						alt={`Go to ${title} Repo`}>Repository</a>
 				</div>
 			</div>
 		</div>
