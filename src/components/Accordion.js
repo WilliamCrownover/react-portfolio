ca4ca221e9f97d5cebf8d2db2f7d2bf628719436
William Crownover <williamcrownover1@gmail.com>
2021-11-01 21:31:39 -0700

    Made the accordion interactive

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,29 @@
+import { useState } from "react";
 
 export default function Accordion() {
+	const [isActive, setIsActive] = useState(false);
 
 	return (
 		<div className='accord-section'>
-			<div className='accord-title'>
+			<div 
+				className='accord-title'
+				onClick={() => setIsActive(!isActive)}
+			>
 				<h3>Front-end Proficiencies</h3>
-				<h3>&gt;</h3>
+				<h3>{isActive ? 'v' : '>' }</h3>
 			</div>
-			<div className='accord-content'>
-				<ul className="proficient-list">
-					<li>HTML5</li>
-					<li>CSS3</li>
-					<li>JavaScript</li>
-					<li>jQuery</li>
-					<li>Handlebars.js</li>
-					<li>React.js</li>
-				</ul>
-			</div>
+			{isActive &&
+				<div className='accord-content'>
+					<ul className="proficient-list">
+						<li>HTML5</li>
+						<li>CSS3</li>
+						<li>JavaScript</li>
+						<li>jQuery</li>
+						<li>Handlebars.js</li>
+						<li>React.js</li>
+					</ul>
+				</div>
+			}
 		</div>
 	);
 
