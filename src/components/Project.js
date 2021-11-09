31d1021b5f725a1c813448e1c4a9be910bb8461a
William Crownover <williamcrownover1@gmail.com>
2021-11-08 18:55:30 -0800

    Redesigning project component and style

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,9 @@ export default function Project( { title, imageName, liveURL, repoURL } ) {
 	return (
 		<div className="project-card">
 			<div className="project-image-btns">
-				<h4>{title}</h4>
+				<div className="project-title">
+					<h3>{title}</h3>
+				</div>
 				<img src={`${process.env.PUBLIC_URL}/assets/${imageName}`} alt="Project"/>
 				<div className="project-btn-wrapper">
 					<a className="btn-left" href={liveURL} target="_blank" rel="noreferrer"
@@ -13,6 +15,9 @@ export default function Project( { title, imageName, liveURL, repoURL } ) {
 					<a className="btn-right" href={repoURL} target="_blank" rel="noreferrer"
 						alt={`Go to ${title} Repo`}>Repository</a>
 				</div>
+			</div>
+			<div className="project-info-card">
+				<p>Testing text</p>
 			</div>
 		</div>
 	);
